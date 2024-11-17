df89770fd2684f49e243653215dcba4df0346564
sinuvasu2888 <sinuvasu288@gmail.com>
2024-11-17 10:47:09 -0500

    Update FirstprojectApplication.java

--- firstproject/src/main/java/com/project/firstproject/FirstprojectApplication.java
+++ firstproject/src/main/java/com/project/firstproject/FirstprojectApplication.java
@@ -11,6 +11,7 @@ public class FirstprojectApplication {
 		SpringApplication.run(FirstprojectApplication.class, args);
 		System.out.println("Hello, World");
 		System.out.println("How are you?");
+		System.out.println("watching chiranjeevi songs");
 	}
 
 }
